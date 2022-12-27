b8a7886f7c867ddcd248dad9ddff04d09983d18e
Carlos Pretelin <carlosppretelin@gmail.com>
2022-12-27 06:37:47 -0600

    Footer css

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import "../styles/Footer.scss"
+import face from "../assets/Footer/fb.svg"
+import insta from "../assets/Footer/insta.svg"
+import twitt from "../assets/Footer/twit.svg"
+import linkd from "../assets/Footer/Linkedin.svg"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='Footer'>
+      
+        
+
+          <div className='Footer_SocialMedia'>
+
+              <div className='Footer_SocialMedia-item'>
+                <img src={face} alt="" />
+              </div>
+
+              <div className='Footer_SocialMedia-item'>
+                <img src={insta} alt="" />
+              </div>
+                
+
+              <div className='Footer_SocialMedia-item'>
+                <img src={twitt} alt="" />
+              </div>
+
+              <div className='Footer_SocialMedia-item'>
+                <img src={linkd} alt="" />
+              </div>
+
+        </div>  
+        <p>Copyright ©2022 All rights reserved</p>
+
+      
+    </div>
   )
 }
 
