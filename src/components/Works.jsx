7f685b55c24fcc536a51a74bc993b32843215ced
Carlos Pretelin <carlosppretelin@gmail.com>
2022-12-27 05:40:43 -0600

    Fetured works section css done

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,10 +8,15 @@ const Works = () => {
     <div className='Works'>
          <img src={workpic} alt="" />
         <p className='Works_title'>Designing Dashboards</p>
-        <span>2020  Dashboard</span>
-        <p className='Works_description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ullam pariatur molestias molestiae obcaecati quaerat id 
-            laborum sunt nisi adipisci iure quis commodi maxime aliquid, distinctio eum sed ab et!</p>
+        <div className='Works-labels'>
+            <span>2020</span>
+            <span>Dashboard</span>
+        </div>
+        
+        <p className='Works_description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ullam pariatur molestias molestiae obcaecati quaerat id </p>
+        <i className="fa-thin fa-horizontal-rule Works-line-icon" />
     </div>
+    
   )
 }
 
