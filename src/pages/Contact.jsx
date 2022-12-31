553a6bfd1b569cbf82db34410088bceaee35ce93
Carlos Pretelin <carlosppretelin@gmail.com>
2022-12-31 05:37:54 -0600

    Simple Contact Page done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,47 +7,41 @@ import "../styles/Contact.scss"
 const Contact = () => {
   return (
     <section className='Contact'>
-      <h1>Contact</h1>
 
-      <div className='Contact-Info'>
-
-        <div className='Contact-Info-Profile'>
-          <img src={ConPic} alt="Profile Picture" />
-
-          <h2>Carlos Pretelin </h2>
-          <p>Junior Front-End Web Developer | HTML/CSS, JAVASCRIPT/REACT.JS | Seeking entry level opportunity</p>
-        </div>
-        
-        
-        <div className='Contact-Wrapper'>
-
-          <div className='Contact-Info-item'>
-            <a href="#">
-              <i className="fa-brands fa-google"></i>
-              <span>E-Mail: carlosppretelin@gmail.com</span>
-            </a>
-            
-          </div>
-
-          <div className='Contact-Info-item'>
-            <a href="https://www.linkedin.com/in/carlos-pretelin/">
-              <i className="fa-brands fa-linkedin"></i>
-              <span>Linkedin/Carlos-Pretelin</span>
-            </a>
-          
-          </div>
-
-          <div className='Contact-Info-item'>
-          <i className="fa-brands fa-github"></i>
-            <span>GitHub/Carlos-Pretelin</span>
-          </div>
-
-          
-
-        </div>
-        
-
+      <div className='Contact-Greeting'>
+        <span>Send me a message!</span>
+        <p>Im looking for an entry level oportunity!</p>
       </div>
+
+
+      <div className='Contact-Socials'>
+        
+        <div className='Contact-Socials-item'>
+          <span><i class="fa-solid fa-envelope"></i> E-Mail</span>
+          <p>carlosppretelin@gmail.com</p>
+        </div>
+
+
+        <div className='Contact-Socials-item'>
+          <span><i class="fa-solid fa-phone"></i> Phone</span>
+          <p>+52 229 530 6311</p>
+        </div>
+
+
+        {/* <div className='Contact-Socials-item'>
+          <h2>Socials</h2>
+          <div className='SocialMedia-Wrapper'>
+            
+          
+
+
+          </div>
+        </div> */}
+
+        
+      </div>
+      
+
     </section>
   )
 }
